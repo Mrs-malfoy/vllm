329351e41bcf2086da6734b9604f646ba423898a
liuboyue <2962116257@qq.com>
2025-01-10 08:56:47 -0900

    modify slo config for 4090

--- vllm/slo_config.py
+++ vllm/slo_config.py
@@ -2,5 +2,5 @@
 class SLOConfig:
     """Dataclass which contains all SLO-related configuration."""
     slo_type_num: int = 3
-    ttft_slos: list[float] = [0.875, 0.450, 0.450]
-    tbt_slos: list[float] = [0.450, 0.450, 0.450]
+    ttft_slos: list[float] = [1, 1, 2]
+    tbt_slos: list[float] = [0.1, 0.5, 0.1]
