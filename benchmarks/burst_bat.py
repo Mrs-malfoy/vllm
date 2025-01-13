ff3802f51a93b6812280dd0bf53618e895c93429
liuboyue <2962116257@qq.com>
2025-01-13 16:43:47 +0000

    use float scale

--- benchmarks/burst_bat.py
+++ benchmarks/burst_bat.py
@@ -5,7 +5,7 @@ def run_benchmark(scale, run_number, num_prompts):
     cmd = f"""python benchmark_serving.py\
              --backend vllm\
              --dataset-name burstgpt\
-             --timestamp-file ./BurstGPT_1.csv\
+             --timestamp-file ./BurstGPT_1000.csv\
              --model Qwen/Qwen2.5-32B-Instruct\
              --ignore-eos\
              --save-result\
@@ -22,10 +22,10 @@ def run_benchmark(scale, run_number, num_prompts):
 
 def main():
     # 定义要测试的request-rate值
-    IRs=[2]
-    start_time = 1625450
-    end_time = 1626512
-    token_num = 1000
+    IRs=[1.4]
+    start_time = 1625686
+    end_time = 1625786
+    num_prompts = 100
 
     # scales = [100, 200, 300]  # 可以根据需要修改
     runs_per_rate = 1  # 每个速率测试5次
@@ -33,7 +33,8 @@ def main():
     
     for ir in IRs:
         target_time = num_prompts / ir
-        scale = int((end_time - start_time) / target_time)
+        scale = float((end_time - start_time) / target_time)
+        print("scale: ", scale)
         for run in range(1, runs_per_rate + 1):
             run_benchmark(scale, run, num_prompts)
         print(f"\n完成 ir {ir} 的所有 {runs_per_rate} 次测试")
